586cdda060ceaf01597643f9801b8b22b17063e5
deshmanea <abhijitdeshmane@gmail.com>
2022-11-05 14:45:04 +0530

    Spring project initial commit

---
+++ src/test/java/com/clipboard/ui/UiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clipboard.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
