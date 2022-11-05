a4b4e43161cfe05c79da348be065bd205e81d8d9
deshmanea <abhijitdeshmane@gmail.com>
2022-11-05 15:55:13 +0530

    Added driver,logger,banner Closes #2,#3,#4

--- src/test/java/com/clipboard/ui/UiAutomationApplicationTests.java
+++ src/test/java/com/clipboard/ui/UiAutomationApplicationTests.java
@@ -1,13 +1,24 @@
 package com.clipboard.ui;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.Assert;
 
 @SpringBootTest
 class UiAutomationApplicationTests {
 
+	@Autowired
+	WebDriver driver;
+
+	@Autowired
+	WebDriverWait wait;
+
 	@Test
-	void contextLoads() {
+	public void testWebdriver(){
+		Assert.assertNotNull(driver);
 	}
 
 }
---
+++ src/test/java/com/clipboard/ui/driver/WebdriverGenerator.java
@@ -0,0 +1,56 @@
+package com.clipboard.ui.driver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+@Configuration
+public class WebdriverGenerator {
+    @Value("${selenium.hub.url}")
+    private String seleniumHubUrl;
+    @Value("${browser}")
+    private String testBrowser;
+    @Value("${wait.time}")
+    private int waitTime;
+    private RemoteWebDriver driver;
+
+    @Bean
+    public RemoteWebDriver getDriver() throws MalformedURLException {
+        if (testBrowser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+            driver = new FirefoxDriver(firefoxOptions);
+        } else {
+            System.setProperty("webdriver.gecko.driver", "chromedriver.exe");
+            driver = new ChromeDriver();
+
+        }
+
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability("browserName", testBrowser);
+//        desiredCapabilities.setCapability("se:recordVideo","true");
+//        driver = new RemoteWebDriver(new URL(seleniumHubUrl), desiredCapabilities);
+
+        return driver;
+    }
+
+    @Lazy
+    @Bean
+    public WebDriverWait getExplicitWait(){
+        return new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+    }
+
+}
